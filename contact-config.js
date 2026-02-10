04bcab2249a03aea32ced701843f04d739699909
BlueprintAiConsulting <drewhufnagle@gmail.com>
2026-02-10 00:59:37 -0500

    Update contact email and phone format across homepage

---
+++ contact-config.js
@@ -0,0 +1,5 @@
+window.CONTACT_CONFIG = {
+  BOOKING_URL: 'https://calendar.app.google/ABoSgYjoUzyC41sD7',
+  EMAIL: 'coachdrew@blueprintaiconsultingco.com',
+  FACEBOOK_MESSENGER_URL: 'https://www.facebook.com/Blueprintaiconsultingco'
+};
